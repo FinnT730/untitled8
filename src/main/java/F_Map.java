b262d339efb804323f3ffaf3b5f38ee74696b89d
FinnT730 <FinnT730@gmail.com>
2020-10-18 20:29:06 +0200

    gg

---
+++ src/main/java/F_Map.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class F_Map<T> {
+
+
+    public F_Map<T> INSTANCE = this;
+
+
+    public List<T> map = new ArrayList<>();
+
+
+}
--- src/main/java/lol.java
+++ src/main/java/lol.java
@@ -2,9 +2,15 @@ public class lol {
 
 
     public static void main(String[] args) {
+
+        F_Map<Integer> test = new F_Map<>();
+
         for(int i = 0; i < 10; i += 1) {
-            System.out.println(i);
+//            System.out.println(i);
+            test.map.add(i);
         }
+
+        test.INSTANCE.INSTANCE.INSTANCE.map.forEach((integer -> System.out.println(integer)));
     }
 
 }
